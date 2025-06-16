e48a4d569d26ae8504da25dbbe9da0d68e625773
fernandocastedo <cmpfernando.ouac@gmail.com>
2025-06-16 16:06:42 -0400

    actual

--- ProyectoFinalUniversidadApp/ViewModels/MainVM.cs
+++ ProyectoFinalUniversidadApp/ViewModels/MainVM.cs
@@ -6,36 +6,7 @@ using System.Threading.Tasks;
 
 namespace ProyectoFinalUniversidadApp.ViewModels
 {
-    public class MainVM : ObservableObject
+    internal class MainVM
     {
-        public ViewModelBase? VistaActual
-        {
-            get => _vistaActual;
-            set => Set(ref _vistaActual, value);
-        }
-        private ViewModelBase? _vistaActual;
-
-        public RelayCommand<string> CambiarVistaCmd { get; }
-
-        private readonly IServiceProvider _sp;
-        public MainVM(IServiceProvider sp)
-        {
-            _sp = sp;
-            CambiarVistaCmd = new RelayCommand<string>(CambiarVista);
-        }
-
-        private void CambiarVista(string token)
-        {
-            VistaActual = token switch
-            {
-                "Materias" => _sp.GetRequiredService<MateriasOfertadasVM>(),
-                "Asistencia" => _sp.GetRequiredService<AsistenciaMateriaVM>(),
-                "Notas" => _sp.GetRequiredService<NotasEstudianteVM>(),
-                "Boletin" => _sp.GetRequiredService<BoletinVM>(),
-                "RegEst" => _sp.GetRequiredService<RegistrarEstudianteVM>(),
-                "RegMat" => _sp.GetRequiredService<RegistrarMateriaVM>(),
-                _ => null
-            };
-        }
     }
 }
---
+++ ProyectoFinalUniversidadApp/Views/Reportes/LoginWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProyectoFinalUniversidadApp.Views.Reportes
+{
+    /// <summary>
+    /// Interaction logic for LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLoginClick(object sender, RoutedEventArgs e)
+        {
+            var username = UsernameTextBox.Text;
+            var password = PasswordBox.Password;
+
+            // Validar usuario
+            if (username == "admin" && password == "admin") // Reemplazar con lógica real
+            {
+                var mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+            }
+            else
+            {
+                System.Windows.MessageBox.Show("Usuario o contraseña incorrectos.");
+            }
+        }
+    }
+}
