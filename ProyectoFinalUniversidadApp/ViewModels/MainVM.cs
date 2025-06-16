7c02e17a7384437b296f8a80aa6f1a9f2c74af6e
fernandocastedo <cmpfernando.ouac@gmail.com>
2025-06-16 14:57:33 -0400

    asd

--- ProyectoFinalUniversidadApp/ViewModels/MainVM.cs
+++ ProyectoFinalUniversidadApp/ViewModels/MainVM.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace ProyectoFinalUniversidadApp.ViewModels
 {
-    internal class MainVM
+    public class MainVM : ObservableObject
     {
+        public ViewModelBase? VistaActual
+        {
+            get => _vistaActual;
+            set => Set(ref _vistaActual, value);
+        }
+        private ViewModelBase? _vistaActual;
+
+        public RelayCommand<string> CambiarVistaCmd { get; }
+
+        private readonly IServiceProvider _sp;
+        public MainVM(IServiceProvider sp)
+        {
+            _sp = sp;
+            CambiarVistaCmd = new RelayCommand<string>(CambiarVista);
+        }
+
+        private void CambiarVista(string token)
+        {
+            VistaActual = token switch
+            {
+                "Materias" => _sp.GetRequiredService<MateriasOfertadasVM>(),
+                "Asistencia" => _sp.GetRequiredService<AsistenciaMateriaVM>(),
+                "Notas" => _sp.GetRequiredService<NotasEstudianteVM>(),
+                "Boletin" => _sp.GetRequiredService<BoletinVM>(),
+                "RegEst" => _sp.GetRequiredService<RegistrarEstudianteVM>(),
+                "RegMat" => _sp.GetRequiredService<RegistrarMateriaVM>(),
+                _ => null
+            };
+        }
     }
 }
